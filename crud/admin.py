29a920c0f36256be1953fdf0438ed8542e5f9a17
Joandarc Zienagebriel <joandarczienagebriel@gmail.com>
2026-06-26 10:14:54 +0300

    change in style

--- crud/admin.py
+++ crud/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from .models import Point, GradeTen
 
+class PointAdmin(admin.ModelAdmin):
+    list_display=[
+        'author',
+        "name",
+        "chemistry",
+        "biology",
+        "math",
+        'physics'
+    ]
+
+class GradeTenAdmin(admin.ModelAdmin):
+    list_display=[
+        'author',
+        "name",
+        "chemistry",
+        "biology",
+        "math",
+        'physics'
+    ]
+
+admin.site.register(Point, PointAdmin)
+admin.site.register(GradeTen, GradeTenAdmin)
 # Register your models here.
